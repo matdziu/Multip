ba6cf3ecfa9c3646d498b2e227ffc339b3ffa532
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2017-07-17 13:30:31 +0200

    Pushed lib code to 'lib' module

--- lib/src/main/java/pl/multip/RecyclerItem.java
+++ lib/src/main/java/pl/multip/RecyclerItem.java
@@ -1,4 +1,4 @@
-package pl.multipexample.lib;
+package pl.multip;
 
 import android.support.annotation.LayoutRes;
 import android.support.v7.widget.RecyclerView;
--- lib/src/main/java/pl/multip/ViewHoldersFactory.java
+++ lib/src/main/java/pl/multip/ViewHoldersFactory.java
@@ -1,4 +1,4 @@
-package pl.multipexample.lib;
+package pl.multip;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
