02e2324cdac7a97e466db8efe077a4eaf11909d5
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2017-07-17 09:30:47 +0200

    Added basic activity and dependencies

---
+++ app/src/main/java/pl/multipexample/MainActivity.java
@@ -0,0 +1,22 @@
+package pl.multipexample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.recycler_view)
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+}
