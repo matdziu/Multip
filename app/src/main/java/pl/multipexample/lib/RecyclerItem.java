f5ec2deb251831fbdb039c5704de7060d50da637
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2017-07-17 10:56:34 +0200

    Added @LayoutRes annotation to getItemViewType()

--- app/src/main/java/pl/multipexample/lib/RecyclerItem.java
+++ app/src/main/java/pl/multipexample/lib/RecyclerItem.java
@@ -1,10 +1,12 @@
 package pl.multipexample.lib;
 
+import android.support.annotation.LayoutRes;
 import android.support.v7.widget.RecyclerView;
 
 public interface RecyclerItem<T extends RecyclerView.ViewHolder> {
 
     void onBindViewHolder(T viewHolder);
 
+    @LayoutRes
     int getItemViewType();
 }
